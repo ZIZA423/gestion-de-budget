77957156c878285ee879b422d375ec0c438dd2cf
@ZIZA423 <azizdiop423@gmail.com>
2022-09-09 12:24:55 +0000

    ma deuxieme commit

---
+++ projet6.js
@@ -0,0 +1,10 @@
+function tester()
+{
+  var dep = document.getElementById('div2').style.display="block";
+  var btn = document.getElementById("butt");
+}
+function teste()
+{
+    var rev = document.getElementById('div2').style.display="block";
+    var btn = document.getElementById("buttt");
+}
